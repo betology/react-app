01ce38edff0f1e85cfcbb669f523a3c0dbdf2f17
b3t0 <betology@gmail.com>
2024-05-31 16:07:04 -0600

    Managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -11,7 +11,7 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item, index) => (
+        {items.map((item) => (
           <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
